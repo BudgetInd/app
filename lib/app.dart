d6eb7d0e92bb7c5b38c0c222d2972b8215de62e4
Call Vin <kanggara75@gmail.com>
2023-12-11 13:14:36 +0700

    add: Auth

---
+++ lib/app.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:budgetin/auth/auth.dart';
+// import 'package:budgetin/login/login.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:budgetin/app/config/config.dart';
+// import 'package:budgetin/dashboard/dashboard.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      // navigatorKey: _navigatorKey,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator().generateRoute,
+      home: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          switch (state) {
+            case AuthAuthenticated _:
+              // return const DashboardPage();
+              return Scaffold(
+                body: Container(
+                  color: Colors.white,
+                  width: MediaQuery.of(context).size.width,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 25.0,
+                        width: 25.0,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.green),
+                          strokeWidth: 4.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            case AuthUnauthenticated _:
+              //   return BlocProvider<LoginBloc>(
+              //     create: (context) => LoginBloc(APILogin()),
+              //     child: const LoginPage(),
+              //   );
+              return Scaffold(
+                body: Container(
+                  color: Colors.white,
+                  width: MediaQuery.of(context).size.width,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 25.0,
+                        width: 25.0,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                          strokeWidth: 4.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            default:
+              return Scaffold(
+                body: Container(
+                  color: Colors.white,
+                  width: MediaQuery.of(context).size.width,
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 25.0,
+                        width: 25.0,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.amber),
+                          strokeWidth: 4.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/app/config/routes.dart
@@ -0,0 +1,70 @@
+import 'package:budgetin/auth/auth.dart';
+// import 'package:budgetin/dashboard/dashboard.dart';
+// import 'package:budgetin/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RouteGenerator {
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => BlocBuilder<AuthBloc, AuthState>(
+            builder: (context, state) {
+              // if (state is AuthAuthenticated) {
+              //   return const DashboardPage();
+              // }
+              // if (state is AuthUnauthenticated) {
+              //   return const LoginPage();
+              // }
+              if (true) {
+                return Scaffold(
+                  body: Container(
+                    color: Colors.white,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 25.0,
+                          width: 25.0,
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.amber),
+                            strokeWidth: 4.0,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+        );
+      // case '/home':
+      //   return MaterialPageRoute(
+      //     builder: (_) => BlocProvider<NavBarBloc>.value(
+      //       value: NavBarBloc(),
+      //       child: const MainPage(),
+      //     ),
+      //   );
+      // case '/sub':
+      //   return MaterialPageRoute(
+      //     builder: (_) => const SubPage(),
+      //   );
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Error'),
+            ),
+            body: const Center(
+              child: Text('ERROR'),
+            ),
+          );
+        });
+    }
+  }
+}
